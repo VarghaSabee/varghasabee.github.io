8618d267ae90cead8fcaab1a94f07f274abbf22d
Varha Sabolch <vargha.sabolch@gmail.com>
2022-12-09 23:33:45 +0100

    deploy config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  assetPrefix: isProd ? "/varghasabee.github.io/" : "",
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
